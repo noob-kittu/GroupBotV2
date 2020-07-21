9df6ffd16818be1e745f3d4935f1e2814f8af645
Kshitij Gagan™ <40312507+KshitijGagan@users.noreply.github.com>
2020-07-21 07:50:01 +0530

    donate updated

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -58,6 +58,7 @@ Hello! my name *{}*.
  - /start: start the bot
  - /help: PM's you this message.
  - /help <module name>: PM's you info about that module.
+ - /donate: information about how to donate!
  - /settings:
    - in PM: will send you your settings for all supported modules.
    - in a group: will redirect you to pm, with all that chat's settings.
@@ -66,6 +67,11 @@ Hello! my name *{}*.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
+
+DONATE_STRING = """Heya, glad to hear you want to donate!
+Janeman is hosted on one of Heroku's Servers and doesn't require any donations as of now but \
+There is only one way of supporting him; [PayPal](paypal.me/KshitijGagan)."""
+
 
 
 IMPORTED = {}
@@ -456,6 +462,26 @@ def get_settings(bot: Bot, update: Update):
     else:
         send_settings(chat.id, user.id, True)
 
+@run_async
+def donate(bot: Bot, update: Update):
+    user = update.effective_message.from_user
+    chat = update.effective_chat  # type: Optional[Chat]
+
+    if chat.type == "private":
+        update.effective_message.reply_text(DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+        if OWNER_ID != 254318997 and DONATION_LINK:
+            update.effective_message.reply_text("You can also donate to the person currently running me "
+                                                "[here]({})".format(DONATION_LINK),
+                                                parse_mode=ParseMode.MARKDOWN)
+
+    else:
+        try:
+            bot.send_message(user.id, DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+            update.effective_message.reply_text("I've PM'ed you about donating to my creator!")
+        except Unauthorized:
+            update.effective_message.reply_text("Contact me in PM first to get donation information.")
 
 
 def migrate_chats(bot: Bot, update: Update):
@@ -492,7 +518,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-   
+    donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
     # dispatcher.add_handler(test_handler)
@@ -505,6 +531,7 @@ def main():
     dispatcher.add_handler(start_callback_handler)
     dispatcher.add_handler(IMDB_HANDLER)
     dispatcher.add_handler(IMDB_SEARCHDATA_HANDLER)
+    dispatcher.add_handler(donate_handler)
     # dispatcher.add_error_handler(error_callback)
 
     if WEBHOOK:
