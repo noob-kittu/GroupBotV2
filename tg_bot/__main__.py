f6073535f02013e6e6ee92dbdcd4ba0b4dd08d1b
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-02-06 08:22:39 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -24,48 +24,32 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 
-
 PM_START_TEXT = """
 
-
-❤❣   (>‿◠)✌   ❤❣
-
-Hello {}, MY NAME IS {} !
-
-I AM A FUNNIEST BUT STRICT GROUP MANAGER BOT
-I am Open Source and my Code can be Found [here](github.com/KshitijGagan/GroupBotV2)
-
-To Donate us Hit /donate
-You can get Support Here [Support Group](t.me/BotSupportGroup)
-
-Click /help or Help button below to find out more about, how to use me to my full potential.
-
+Hi {}, my name is {}! 
+I am an Anime themed group management bot.
+Build by weebs for weebs, I specialize in managing anime and similar themed groups.
+You can find my list of *available commands* with :
+👉 /help.
 
 """
 
 HELP_STRINGS = """
 
-Hello! my name *{}*.
-
+Hey there! My name is *{}*.
+I am a blank, I will be your partner and can help make your group great and attract members to the group. Take a look to know about some of the following things.
 *Main* commands available:
- - /start: start the bot
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
-
+ • /help: PM's you this message.
+ • /help <module name>: PM's you info about that module.
+ • /settings:
+   • in PM: will send you your settings for all supported modules.
+   • in a group: will redirect you to pm, with all that chat's settings.
 
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-Currently, I am hosted on one of Heroku's Servers and doesn't require any donations as of now but \
-You can Donante of supporting him; [PayPal](paypal.me/KshitijGagan)."""
-
-
+KORA_IMG = "https://telegra.ph/file/8fda57770ba246a91befe.jpg"
 
 IMPORTED = {}
 MIGRATEABLE = []
@@ -77,6 +61,7 @@ DATA_EXPORT = []
 
 CHAT_SETTINGS = {}
 USER_SETTINGS = {}
+
 
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("tg_bot.modules." + module_name)
@@ -153,14 +138,25 @@ def start(bot: Bot, update: Update, args: List[str]):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_text(
-                PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="ADD ME TO YOUR GROUP",
-                                                                       url="t.me/{}?startgroup=true".format(bot.username))]]))
-
+            update.effective_message.reply_photo(
+                KORA_IMG,
+                PM_START_TEXT.format(
+                    escape_markdown(first_name), 
+                    escape_markdown(bot.first_name), OWNER_ID),
+                parse_mode=ParseMode.MARKDOWN,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(
+               [[
+               InlineKeyboardButton(
+                   text="👑 SUMMON ME",
+                   url="t.me/{}?startgroup=true".format(bot.username)),
+               InlineKeyboardButton(
+                             text="⚡️ Get Support",
+                             url=f"https://t.me/KoraSupport")
+               ]]))
 
     else:
-        update.effective_message.reply_text("Yo, whadup?🤧")
+        update.effective_message.reply_text("Hey, I'm Online.")
 
 
 def send_start(bot, update):
@@ -455,26 +451,6 @@ def get_settings(bot: Bot, update: Update):
     else:
         send_settings(chat.id, user.id, True)
 
-@run_async
-def donate(bot: Bot, update: Update):
-    user = update.effective_message.from_user
-    chat = update.effective_chat  # type: Optional[Chat]
-
-    if chat.type == "private":
-        update.effective_message.reply_text(DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
-
-        if OWNER_ID != 866838349 and DONATION_LINK:
-            update.effective_message.reply_text("You can also donate to the person currently running me "
-                                                "[here]({})".format(DONATION_LINK),
-                                                parse_mode=ParseMode.MARKDOWN)
-
-    else:
-        try:
-            bot.send_message(user.id, DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
-
-            update.effective_message.reply_text("I've PM'ed you about donating to my creator!")
-        except Unauthorized:
-            update.effective_message.reply_text("Contact me in PM first to get donation information.")
 
 
 def migrate_chats(bot: Bot, update: Update):
@@ -511,7 +487,7 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    donate_handler = CommandHandler("donate", donate)
+   
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
     # dispatcher.add_handler(test_handler)
@@ -524,7 +500,6 @@ def main():
     dispatcher.add_handler(start_callback_handler)
     dispatcher.add_handler(IMDB_HANDLER)
     dispatcher.add_handler(IMDB_SEARCHDATA_HANDLER)
-    dispatcher.add_handler(donate_handler)
     # dispatcher.add_error_handler(error_callback)
 
     if WEBHOOK:
